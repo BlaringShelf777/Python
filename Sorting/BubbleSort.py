a5968aab9716ae04c4697f74a126a5e3cd37ea16
BlaringShelf777 <56661141+BlaringShelf777@users.noreply.github.com>
2021-02-23 02:54:58 -0300

    BubleSort

---
+++ Sorting/BubbleSort.py
@@ -0,0 +1,15 @@
+
+def bubbleSort(array):
+    for j in range(len(array) - 1, 0, -1):
+        for i in range(j):
+            if array[i] > array[i+1]:
+                temp = array[i]
+                array[i] = array[i+1]
+                array[i+1] = temp
+
+
+array = [54,26,93,17,77,31,44,55,20]
+
+print('> Array not sorted:', array)
+bubbleSort(array)
+print('> Array sorted:', array)
