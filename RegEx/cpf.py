8316bf28dcfb7423b76ced2405db52e74073394f
BlaringShelf777 <56661141+BlaringShelf777@users.noreply.github.com>
2021-03-05 21:26:00 -0300

    A sequence of one number doesnt belong to our reg. lang.

--- RegEx/cpf.py
+++ RegEx/cpf.py
@@ -1,11 +1,16 @@
 import re
 
-validate = re.compile(r'^\d{3}\.\d{3}\.\d{3}-\d{2}$')
+#validate = re.compile(r'^\d{3}\.\d{3}\.\d{3}-\d{2}$')
 noFormat = re.compile(r'^(\d{3})\.(\d{3})\.(\d{3})-(\d{2})$')
+validate = re.compile(
+    r'^(?!(\d)\1{2}\.\1{3}\.\1{3}-\1{2})(\d{3}\.\d{3}\.\d{3}-\d{2})$'
+)
 
 
-cpf = '090.890.410-10'
+cpf1 = '090.890.410-10'
+cpf2 = '111.111.111-11'
 
 
-print(validate.findall(cpf))
-print(noFormat.sub(r'\1\2\3\4', cpf))
+print(validate.findall(cpf1)[0][1])
+print(validate.findall(cpf2))
+print(noFormat.sub(r'\1\2\3\4', cpf1))
